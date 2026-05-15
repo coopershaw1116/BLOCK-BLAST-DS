bf62ab73c00e066933f9a7ee92c86f77935cee38
coopershaw1116 <coopershawprofessional@gmail.com>
2026-05-15 20:28:59 +1000

    Added all piece types from Block Blast!

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/coopershaw1116/block-blast-ds
+# Install script for directory: /home/coopershaw1116/BLOCK-BLAST-DS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/coopershaw1116/block-blast-ds/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/coopershaw1116/BLOCK-BLAST-DS/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
